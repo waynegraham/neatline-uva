c018338cffee151de5d1320d306a28be0deb8516
Wayne Graham <wayne.graham@virginia.edu>
2014-05-05 11:39:55 -0400

    Basic app to display active projects
    Display faculty Neatline projects

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+$all_dirs = scandir('/usr/local/projects');
+$hide_dirs = array(
+  '.',
+  '..',
+  'workshop',
+  'current',
+  'geoserver',
+  'henshaw',
+  'holsinger',
+  'hotchkiss',
+  'lovecraft',
+  'maps',
+  'neatline',
+  'neatline-uva.org',
+  'sandbox',
+  'to_delete'
+);
+$dirs = array_diff($all_dirs, $hide_dirs);
+?>
+
+
+<!doctype html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+<ul>
+<?php foreach($dirs as $dir): ?>
+  <li><a href="http://<?php echo $dir;  ?>.neatline-uva.org/"><?php echo $dir ?></li>
+<?php endforeach; ?>
+</ul>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-50671828-1', 'neatline-uva.org');ga('send', 'pageview');
+</script>
+    </body>
+</html>
